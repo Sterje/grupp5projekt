2a1bf73cddb112f9da430a0852bcbfa75860d2a2
Jesper Eriksson Stenberg <jespermail@gmail.com>
2024-12-17 14:57:29 +0100

    UI

---
+++ script.js
@@ -0,0 +1,24 @@
+function popup () {
+        myPopup.classList.add("show");
+    }
+;
+closePopup.addEventListener(
+    "click",
+    function () {
+        myPopup.classList.remove(
+            "show"
+        );
+    }
+);
+window.addEventListener(
+    "click",
+    function (event) {
+        if (event.target == myPopup) {
+            myPopup.classList.remove(
+                "show"
+            );
+        }
+    }
+);
+
+popup()
