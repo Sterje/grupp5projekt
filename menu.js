91740a208e4324208bb6bd9ea325b81a1d7c12e2
Luckmore83 <daaksve@gmail.com>
2024-12-17 14:09:14 +0100

    First commit of featured food and drink

---
+++ menu.js
@@ -0,0 +1,220 @@
+const db = {
+    "bbqs": [
+      {
+        "id": "ribs-brisket-and-burnt-ends",
+        "img": "https://goldbelly.imgix.net/uploads/showcase_media_asset/image/79619/joes-kc-ribs-brisket-and-burnt-ends.6710e994980e485e6441b794717ad6fb.jpg?ixlib=react-9.0.2&auto=format&ar=1%3A1",
+        "name": "Joe's KC BBQ",
+        "dsc": "Joe's KC Ribs, Brisket & Burnt Ends",
+        "price": 110.99,
+        "rate": 4,
+        "country": "Kansas City, KS"
+      },
+      {
+        "id": "005-kings-carolina-oink-sampler",
+        "img": "https://goldbelly.imgix.net/uploads/showcase_media_asset/image/66752/carolina-bbq-oink-sampler.1340b5a10cedc238cb2280306dd1d5a5.jpg?ixlib=react-9.0.2&auto=format&ar=1%3A1",
+        "name": "Kings BBQ",
+        "dsc": "Carolina BBQ Oink Sampler",
+        "price": 89,
+        "rate": 4,
+        "country": "Kinston, NC"
+      },
+      {
+        "id": "texas-monthlys-1-bbq-brisket",
+        "img": "https://goldbelly.imgix.net/uploads/showcase_media_asset/image/131249/texas-monthlys-1-bbq-brisket.1006a061be7acae03992e420fbca995a.png?ixlib=react-9.0.2&auto=format&ar=1%3A1",
+        "name": "Snow's BBQ",
+        "dsc": "Texas Monthly's #1 BBQ Brisket",
+        "price": 199,
+        "rate": 4,
+        "country": "Lexington, TX"
+      },
+      {
+        "id": "17352-ribs-and-pulled-pork-small-combo-pack",
+        "img": "https://goldbelly.imgix.net/uploads/showcase_media_asset/image/137366/Central-BBQ-Ribs-Pulled-Pork-Dinner-1.jpg?ixlib=react-9.0.2&auto=format&ar=1%3A1",
+        "name": "Central BBQ",
+        "dsc": "Ribs & Pulled Pork Dinner for 4",
+        "price": 79,
+        "rate": 4,
+        "country": "Memphis, TN"
+      },
+      {
+        "id": "whole-brisket-texas-bbq-sauce",
+        "img": "https://goldbelly.imgix.net/uploads/showcase_media_asset/image/110384/whole-brisket-texas-barbecue-bbq-sauce.e07ee4818b20ed43d217bf67fedd41ce.jpg?ixlib=react-9.0.2&auto=format&ar=1%3A1",
+        "name": "Franklin Barbecue",
+        "dsc": "Whole Brisket + Texas Barbecue BBQ Sauce",
+        "price": 249,
+        "rate": 4,
+        "country": "Austin, TX"
+      }
+    ],
+  "burgers": [
+      {
+        "id": "the-gramercy-tavern-burger-4-pack",
+        "img": "https://goldbelly.imgix.net/uploads/showcase_media_asset/image/137148/Gramercy-Tavern-Burger-and-Kielbasa-Kit-6.4.21-72ppi-1x1-15.jpg?ixlib=react-9.0.2&auto=format&ar=1%3A1",
+        "name": "Gramercy Tavern",
+        "dsc": "The Gramercy Tavern Burger - 4 Pack",
+        "price": 99,
+        "rate": 5,
+        "country": "New York, NY"
+      },
+      {
+        "id": "shake-shack-shackburger-8-pack",
+        "img": "https://goldbelly.imgix.net/uploads/showcase_media_asset/image/134862/shake-shack-shackburger-8-pack.973a5e26836ea86d7e86a327becea2b0.png?ixlib=react-9.0.2&auto=format&ar=1%3A1",
+        "name": "Shake Shack",
+        "dsc": "Shake Shack ShackBurger® – 8 Pack",
+        "price": 49,
+        "rate": 5,
+        "country": "New York, NY"
+      },
+      {
+        "id": "gotts-cheeseburger-kit-for-4",
+        "img": "https://goldbelly.imgix.net/uploads/showcase_media_asset/image/132933/gotts-complete-cheeseburger-kit-for-4.7bdc74104b193427b3fe6eae39e05b5e.jpg?ixlib=react-9.0.2&auto=format&ar=1%3A1",
+        "name": "Gott's Roadside",
+        "dsc": "Gott's Complete Cheeseburger Kit for 4",
+        "price": 99,
+        "rate": 5,
+        "country": "St. Helena, CA"
+      },
+      {
+        "id": "wagyu-burger-patties-12-pack",
+        "img": "https://goldbelly.imgix.net/uploads/showcase_media_asset/image/107019/wagyu-burger-patties-12-pack.6116f4cd648dee20651f99e21e7d758b.jpg?ixlib=react-9.0.2&auto=format&ar=1%3A1",
+        "name": "Westholme Wagyu",
+        "dsc": "Wagyu Burger Patties - 12 Pack",
+        "price": 129,
+        "rate": 5,
+        "country": "Queensland, Australia"
+     },
+      {
+        "id": "shake-shack-shackburger-16-pack",
+        "img": "https://goldbelly.imgix.net/uploads/showcase_media_asset/image/134022/shake-shack-shackburger-16-pack.316f8b09144db65931ea29e34869287a.png?ixlib=react-9.0.2&auto=format&ar=1%3A1",
+        "name": "Shake Shack",
+        "dsc": "Shake Shack Shackburger® – 16 Pack",
+        "price": 89,
+        "rate": 4,
+        "country": "New York, NY"
+      }
+    ],
+  "desserts": [
+      {
+        "id": "15259-german-chocolate-killer-brownie-tin-pack",
+        "img": "https://goldbelly.imgix.net/uploads/showcase_media_asset/image/132029/german-chocolate-killer-brownie-tin-pack.5ebc34160f28767a9d94c4da2e04c4b9.jpg?ixlib=react-9.0.2&auto=format&ar=1%3A1",
+        "name": "Killer Brownie®",
+        "dsc": "German Chocolate Killer Brownie®",
+        "price": 39.99,
+        "rate": 4,
+        "country": "Dayton, OH"
+      },
+      {
+        "id": "world-famous-banana-pudding-party-sized",
+        "img": "https://goldbelly.imgix.net/uploads/showcase_media_asset/image/84893/world-famous-banana-pudding-bowl-64-oz.85af650f8f51512f8f3181a11d6587d6.jpg?ixlib=react-9.0.2&auto=format&ar=1%3A1",
+        "name": "Magnolia Bakery",
+        "dsc": "World Famous Banana Pudding Bowl (64 oz.)",
+        "price": 56,
+        "rate": 4,
+        "country": "New York, NY"
+      },
+      {
+        "id": "jacques-world-famous-chocolate-chip-cookies",
+        "img": "https://goldbelly.imgix.net/uploads/showcase_media_asset/image/81172/jacques-world-famous-chocolate-chip-cookies-6-pack.2217a14c443602493bba88aa9335319a.jpg?ixlib=react-9.0.2&auto=format&ar=1%3A1",
+        "name": "Jacques Torres Chocolate",
+        "dsc": "Jacques’ World Famous Chocolate Chip Cookies - 6 Pack",
+        "price": 39.95,
+        "rate": 4,
+        "country": "New York, NY"
+      },
+      {
+        "id": "luigis-original-cannoli-pie",
+        "img": "https://goldbelly.imgix.net/uploads/showcase_media_asset/image/132206/original-cannoli-pie.4cb5b9ba82f57b69b90765fd9f07aa1a.jpg?ixlib=react-9.0.2&auto=format&ar=1%3A1",
+        "name": "The Cannoli Pie Company",
+        "dsc": "Original Cannoli Pie",
+        "price": 69,
+        "rate": 4,
+        "country": "Bridgeport, CT"
+      },
+      {
+        "id": "sea-salted-caramel-swirl-cheesecake",
+        "img": "https://goldbelly.imgix.net/uploads/showcase_media_asset/image/134006/sea-salted-caramel-swirl-cheesecake.e2825335433fb7a272a5d77649a6849e.jpg?ixlib=react-9.0.2&auto=format&ar=1%3A1",
+        "name": "Cotton Blues Cheesecake Company",
+        "dsc": "Sea-Salted Caramel Swirl Cheesecake",
+        "price": 65,
+        "rate": 4,
+        "country": "Hattiesburg, MS"
+      }
+    ],
+  "drinks": [
+      {
+        "id": "hong-kong-boba-tea-kit-for-6",
+        "img": "https://goldbelly.imgix.net/uploads/showcase_media_asset/image/108725/hong-kong-boba-tea-kit-for-6.63841de36d8e5edfafa13023fc303285.jpg?ixlib=react-9.0.2&auto=format&ar=1%3A1",
+        "name": "New Territories",
+        "dsc": "Hong Kong Boba Tea Kit for 6",
+        "price": 59,
+        "rate": 4,
+        "country": "New York, NY"
+      },
+      {
+        "id": "guys-caliente-margaritas-for-12",
+        "img": "https://goldbelly.imgix.net/uploads/showcase_media_asset/image/126836/guys-caliente-margaritas-for-12.ca8c6bc06b8f1039549385ffcebc749d.jpg?ixlib=react-9.0.2&auto=format&ar=1%3A1",
+        "name": "Guy Fieri",
+        "dsc": "Guy's Caliente Margaritas for 12",
+        "price": 69,
+        "rate": 5,
+        "country": "Flavortown, USA"
+      },
+      {
+        "id": "woodford-reserve-mint-julep-syrup",
+        "img": "https://goldbelly.imgix.net/uploads/showcase_media_asset/image/134036/woodford-reserve-mint-julep-syrup.ef523ac7cbae5f4aba6b058207f490d2.jpg?ixlib=react-9.0.2&auto=format&ar=1%3A1",
+        "name": "Woodford Reserve",
+        "dsc": "Woodford Reserve Mint Julep Syrup",
+        "price": 39,
+        "rate": 4,
+        "country": "Louisville, KY"
+      },
+      {
+        "id": "new-orleans-hurricane-mix",
+        "img": "https://goldbelly.imgix.net/uploads/showcase_media_asset/image/84522/new-orleans-hurricane-mix.4613584fc65cb0787024dd24d2a8f4b3.jpg?ixlib=react-9.0.2&auto=format&ar=1%3A1",
+        "name": "Franco's Hurricane Mix",
+        "dsc": "New Orleans Hurricane Mix",
+        "price": 39,
+        "rate": 5,
+        "country": "Natchitoches, LA"
+      },
+      {
+        "id": "evan-williams-gourmet-mint-julep-mix-2-pack",
+        "img": "https://goldbelly.imgix.net/uploads/product_image/image/42820/evan-williams-gourmet-mint-julep-mix-2-pack.a0bd561099dd14bfb33e3363c1c025d0.jpg?ixlib=react-9.0.2&auto=format&ar=1%3A1",
+        "name": "Evan Williams",
+        "dsc": "Evan Williams Gourmet Mint Julep Mix - 2 Pack",
+        "price": 39,
+        "rate": 4,
+        "country": "Louisville, KY"
+      }
+    ],
+  "pagination": {
+      "bbqs": 5,
+      "burgers": 5,
+      "desserts": 5,
+      "drinks": 5
+    }
+  }
+
+  const mainDishes = [...db.bbqs, ...db.burgers];
+const drinkItems = db.drinks;
+
+function getRandomItem(array) {
+    return array[Math.floor(Math.random() * array.length)];
+}
+
+function displayFeaturedItems() {
+    const featuredFood = getRandomItem(mainDishes);
+    const featuredDrink = getRandomItem(drinkItems);
+
+    document.getElementById('featured-food').innerHTML = `
+        <img src="${featuredFood.img}" alt="${featuredFood.name}">
+        <h3>${featuredFood.name}</h3>
+    `;
+
+    document.getElementById('featured-drink').innerHTML = `
+        <img src="${featuredDrink.img}" alt="${featuredDrink.name}">
+        <h3>${featuredDrink.name}</h3>
+    `;
+}
+
+window.onload = displayFeaturedItems;
